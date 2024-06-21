184e8fe59452daa034b59c5bf902cb36d2fa4115
gui578 <arrivabene77@gmail.com>
2024-06-21 09:09:52 +0900

    Project's Initial Commit

---
+++ www/components/loader.js
@@ -0,0 +1,22 @@
+// This is a JavaScript file
+
+$(document).on("click", "#camera", function(){
+
+navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
+    destinationType: Camera.DestinationType.FILE_URI,
+
+    correct0rientation:true,
+    saveToPhotoAlbum:true
+
+    });
+    
+function onSuccess(imageURI) {
+    var image = document.getElementById('myImage');
+    image.src = imageURI;
+}
+
+function onFail(message) {
+    alert('Failed because: ' + message);
+}
+
+});
